27c480af473b30d1f3e087669700db6c3d46220b
Daniel Stolpe <daniel.stolpe@gmx.de>
2015-03-13 10:31:15 +0100

    Another try with static url, ref #20

--- eventbooking/urls.py
+++ eventbooking/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,3 +11,5 @@ urlpatterns = patterns('',
     url(r'^zeltlager/', include('zeltlager_registration.urls')),
     (r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views.serve', {'document_root': settings.STATIC_ROOT}),
 )
+
+
